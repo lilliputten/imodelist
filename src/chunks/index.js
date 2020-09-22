9eac2c8121d07fe7ea8653524367ed694530e3c4
uknowmyname <Uknowmylifeisshit@gmail.com>
2020-09-22 13:50:41 +0300

    +

---
+++ src/chunks/index.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import 'bootstrap';
+window.jQuery = $;
+window.$ = $;
+require ('@fancyapps/fancybox');
+require('../scss/styles.scss');
+
+
+$(function () {
+  //tooltip
+  $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+  //fancybox
+  $("[data-fancybox]").fancybox();
+  //toggle mobile menu
+  $("a[href='#toggle-main-left-menu']").click(function(){
+    $(this).toggleClass('active');
+    $('.main-left-menu').toggleClass('active');
+    $('body, html').toggleClass('scroll-lock');
+  });
+  //toggle mobile cata menu
+  $("a[href='#toggle-cata-left-menu']").click(function(){
+    $(this).toggleClass('active');
+    $('.cata-left-menu').toggleClass('active');
+    $('body, html').toggleClass('scroll-lock');
+  });
+});
---
+++ src/chunks/utils.js
@@ -0,0 +1,61 @@
+function formatNumber(num)
+{
+  num = num.replace(/\D/g, '');
+  return ("" + num).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, function($1) { return $1 + " " });
+}
+$.fn.thousandSeparate = function () {
+  return this.each(function () {
+    var $input = $(this);
+    $input.bind('keyup', function (e) {
+      $input.val(formatNumber($input.val()));
+    });
+  });
+};
+$.fn.thousandSeparateString = function () {
+  this.text(formatNumber(this.text()));
+};
+
+$(function () {
+  var $body = $('body');
+
+  //modal
+  $body.on('click', '.ajax-modal', function(e){
+    e.preventDefault();
+    var $obj = $(this).data("target");
+    $($obj+' .modal-content').load($(this).data("remote"),function(){
+      $($obj).modal({show:true});
+    });
+  });
+  //thousand separate
+  $('[data-thousand-separate]').each(function(){
+    $(this).thousandSeparateString();
+  });
+
+  var slider = $('.row-scroll-always');
+  var isDown = false;
+  var startX;
+  var scrollLeft;
+
+  slider.on('mousedown', (e) => {
+    isDown = true;
+    slider.addClass('active');
+    startX = e.pageX - slider.offset().left;
+    scrollLeft = slider.scrollLeft();
+    console.log(slider.scrollLeft());
+  });
+  slider.on('mouseleave', () => {
+    isDown = false;
+    slider.removeClass('active');
+  });
+  slider.on('mouseup', () => {
+    isDown = false;
+    slider.removeClass('active');
+  });
+  slider.on('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offset().left;
+    const walk = (x - startX) * 2; //scroll-fast
+    slider.scrollLeft(scrollLeft - walk);
+  });
+});
