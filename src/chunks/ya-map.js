393285609593930e9a25e1f501d9fd9a30c7c6ab
lilliputten <lilliputten@gmail.com>
2024-12-11 01:59:34 +0300

    Issue #2: Added Magnit Post delivery points map on the `lk-cart-delivery.ejs` page.

--- src/chunks/ya-map.js
+++ src/chunks/ya-map.js
@@ -1,32 +1,31 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
   if ($('.map-holder').length) {
-    $('.map-holder').find('input.autoComplete').on('autocomplete.select', function(evt, item) {
-      var post = $(this).data('post');
-      var input = $(this).val();
-      $.ajax( {
-        type: "get",
-        url: post,
-        data: {id: input},
-        success: function( response ) {
-          try {
-            response = JSON.parse(response)
-            $('.title').html(response.title);
-            $('.text').html(response.text);
-            $('.close-map').addClass('btn').text('Выбрать');
-            $('.close-map').data('uid', response.uid);
-            $('.close-map').data('title', response.title);
-          } catch(e) {}
-        }
+    $('.map-holder')
+      .find('input.autoComplete')
+      .on('autocomplete.select', function (evt, item) {
+        var post = $(this).data('post');
+        var input = $(this).val();
+        $.ajax({
+          type: 'get',
+          url: post,
+          data: { id: input },
+          success: function (response) {
+            try {
+              response = JSON.parse(response);
+              $('.title').html(response.title);
+              $('.text').html(response.text);
+              $('.close-map').addClass('btn').text('Выбрать');
+              $('.close-map').data('uid', response.uid);
+              $('.close-map').data('title', response.title);
+            } catch (e) {}
+          },
+        });
       });
-    })
-
-
 
     ymaps.ready(init);
     var ALLmaps = [];
-    function init(){
-      $('.map-holder').each(function() {
+    function init() {
+      $('.map-holder').each(function () {
         var map = $(this).data('map');
         var obj = $('#' + map);
         var zoom = obj.data('zoom');
@@ -36,7 +35,7 @@ $(document).ready(function() {
         var lg = obj.data('lg');
         var myMap = new ymaps.Map(map, {
           center: [lt, lg],
-          zoom: zoom
+          zoom: zoom,
         });
         ALLmaps.push(myMap);
         getAjax();
@@ -45,10 +44,10 @@ $(document).ready(function() {
           getAjax();
         });
 
-        function getAjax(){
+        function getAjax() {
           var bounds = myMap.getBounds();
           $.ajax({
-            type: "GET",
+            type: 'GET',
             url: url,
             data: {
               l: bounds[0],
@@ -57,23 +56,28 @@ $(document).ready(function() {
             },
             success: function (response) {
               try {
-                response=JSON.parse(response);
-                myCollection = new ymaps.GeoObjectCollection({}, {
-                  preset: 'islands#redCircleDotIcon'
-                });
+                if (typeof response === 'string') {
+                  response = JSON.parse(response);
+                }
+                myCollection = new ymaps.GeoObjectCollection(
+                  {},
+                  {
+                    preset: 'islands#redCircleDotIcon',
+                  },
+                );
                 for (var i = 0; i < response.length; i++) {
                   var myGeoObject = new ymaps.GeoObject({
                     geometry: {
-                      type: "Point",
-                      coordinates: [+response[i].lt, +response[i].lg]
+                      type: 'Point',
+                      coordinates: [+response[i].lt, +response[i].lg],
                     },
                     properties: {
                       hintContent: response[i].title,
-                    }
+                    },
                   });
                   (function () {
                     var resp = response[i];
-                    myGeoObject.events.add("click", function (e) {
+                    myGeoObject.events.add('click', function (e) {
                       $('.title').html(resp.title);
                       $('.text').html(resp.text);
                       $('.close-map').addClass('btn').text('Выбрать');
@@ -84,42 +88,47 @@ $(document).ready(function() {
                   myCollection.add(myGeoObject);
                 }
                 myMap.geoObjects.add(myCollection);
-              } catch(e) {}
-            }
+              } catch (e) {
+                console.error('[ya-map:init]', e);
+                debugger;
+              }
+            },
           });
         }
       });
     }
-    $('.close-map').click(function(e){
+    $('.close-map').click(function (e) {
       e.preventDefault();
       $('[name="uid"]').val($('.close-map').data('uid'));
       $('.close-map').closest('.map-accord').addClass('disabled');
       $('.show-deliver-point').removeClass('disabled');
       $('.show-deliver-point .title').html($('.close-map').data('title'));
       $('.show-deliver-point .text').html($('.close-map').data('text'));
-    })
-    $('.map-tabs').click(function(e){
+    });
+    $('.map-tabs').click(function (e) {
       for (var i = 0; i < ALLmaps.length; i++) {
-        (function() {
+        (function () {
           var m = ALLmaps[i];
           setTimeout(function () {
             m.container.fitToViewport();
-          }, 300)
+          }, 300);
         })(i);
       }
-    })
-    $('input[name="product_order[delivery_form]"]').change(function(e){
+    });
+    $('input[name="product_order[delivery_form]"]').change(function (e) {
       if ($(this).hasClass('map-tab-active')) {
         $('[name="uid"]').val('');
         $('.show-deliver-point').addClass('disabled');
         $('.map-accord').find('.tab-pane').removeClass('active');
-        $('.map-accord').find('#'+$(this).data('map')).addClass('active');
+        $('.map-accord')
+          .find('#' + $(this).data('map'))
+          .addClass('active');
         $('.map-accord').removeClass('disabled');
       } else {
         $('[name="uid"]').val('');
         $('.map-accord').addClass('disabled');
         $('.show-deliver-point').addClass('disabled');
       }
-    })
+    });
   }
 });
