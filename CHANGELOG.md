1f704ea9c88217fd669d1355494828d279508f4a
lilliputten <lilliputten@gmail.com>
2025-04-16 17:58:32 +0300

    Issue #5: Added bottom notification panel with a private data usage warning. Updated version: 1.0.4 .

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+<!--
+ @since 2025.04.16
+ @changed 2025.04.16, 17:45
+-->
+
+# CHANGELOG
+
+## [v.1.0.4](https://github.com/lilliputten/imodelist/releases/tag/v.1.0.3) - 2025.04.16
+
+- Added bottom notification panel with a private data usage warning.
+
+See also:
+
+- [Issue #5: Add personal data and cookies announcement](https://github.com/lilliputten/imodelist/issues/5)
+- [Compare with the previous version](https://github.com/lilliputten/imodelist/compare/v.1.0.3...v.1.0.4)
+- [Donwload published build](https://github.com/lilliputten/imodelist/archive/refs/tags/publish.1.0.3.zip)
+
+## [v.1.0.3](https://github.com/lilliputten/imodelist/releases/tag/v.1.0.3) - 2025.03.25
+
+- Added new icon skeletons (for tg, dzen, rutube; the last 2 ones are non-implemented yet).
+
+See also:
+
+- [Issue #3: Update social icons](https://github.com/lilliputten/imodelist/issues/3)
+- [Compare with the previous version](https://github.com/lilliputten/imodelist/compare/v.1.0.2...v.1.0.3)
+- [Donwload published build](https://github.com/lilliputten/imodelist/archive/refs/tags/publish.1.0.3.zip)
+
---
+++ src/chunks/bottom-notification.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  // Fond an announcement panel node in dom
+  const node = document.getElementById('bottom-notification');
+
+  // 'Already confirmed' state in the local storage
+  const confirmedId = 'bottom-notification-confirmed';
+
+  // Check if it has been confirmed already?
+  const hasConfirmed = !window.localStorage || !!window.localStorage.getItem(confirmedId);
+  if (hasConfirmed || !node) {
+    if (node) {
+      node.remove();
+    }
+    return;
+  }
+
+  // Show the panel
+  node.classList.toggle('visible', true);
+  // Set the body class
+  document.body.classList.toggle('with-bottom-notification', true);
+
+  // Fond other essential dom nodes
+  const button = document.getElementById('bottom-notification-confirm');
+  const footer = document.getElementById('footer');
+  const wrapper = node.querySelector('.bottom-notification-wrapper');
+
+  function updateGeometry() {
+    // Update footer's bottom padding
+    const nodeHeight =
+      document.body.classList.contains('with-bottom-notification') && wrapper
+        ? wrapper.offsetHeight
+        : 0;
+    footer.style.paddingBottom = nodeHeight + 'px';
+  }
+
+  function onButtonClick() {
+    // Ok, confirmed. Restore the original state, remove the panel and set the local storage flag
+    window.localStorage.setItem(confirmedId, 'yes');
+    document.body.classList.toggle('with-bottom-notification', false);
+    window.removeEventListener('resize', updateGeometry);
+    node.remove();
+    updateGeometry();
+  }
+
+  // Add essential events
+  button.addEventListener('click', onButtonClick);
+  window.addEventListener('resize', updateGeometry);
+
+  // Initially update geometry
+  if (window.requestAnimationFrame) {
+    window.requestAnimationFrame(updateGeometry);
+  } else {
+    setTimeout(updateGeometry, 50);
+  }
+});
